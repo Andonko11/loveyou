56512d57973b1f1f73c4fe97f3c1056b6264618d
unknown <zontak@NTB-139.scalefocus.com>
2026-03-24 19:30:06 +0200

    adding music

--- script.js
+++ script.js
@@ -2,21 +2,32 @@ $(document).ready(function () {
   var envelope = $("#envelope");
   var btn_open = $("#open");
   var btn_reset = $("#reset");
+  var music = document.getElementById("music");
 
   envelope.click(function () {
     open();
   });
+
   btn_open.click(function () {
     open();
   });
+
   btn_reset.click(function () {
     close();
   });
 
   function open() {
     envelope.addClass("open").removeClass("close");
+    
+    // пуска музиката
+    music.play();
   }
+
   function close() {
     envelope.addClass("close").removeClass("open");
+    
+    // спира музиката и връща отначало
+    music.pause();
+    music.currentTime = 0;
   }
 });
